f0edc9962558bbc75017fab1a352d6566eb5dbb4
Sadia Zafreen <sadiazafreen525@gmail.com>
2026-08-18 21:11:32 +0530

    Add files via upload

---
+++ PacMan/pacman.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("animations");
+const ghostFrames = document.getElementById("ghosts");
+
+let createRect = (x, y, width, height, color) => {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+};
+
+let map = []
